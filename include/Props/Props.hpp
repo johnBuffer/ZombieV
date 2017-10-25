df5e50a011bf9ed0fc17abe3df6ba1df36abe699
jean <jean.tampon@intempora.com>
2017-10-25 17:47:18 +0200

    added props

---
+++ include/Props/Props.hpp
@@ -0,0 +1,10 @@
+#ifndef PROPS_HPP_INCLUDED
+#define PROPS_HPP_INCLUDED
+
+#include "Guts.hpp"
+#include "Fire.hpp"
+#include "Smoke.hpp"
+#include "BulletShell.hpp"
+#include "ExplosionProvider.hpp"
+
+#endif // PROPS_HPP_INCLUDED
